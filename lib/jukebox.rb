addfd2085f9f0b7235ad7e5ba79f8b5b829bab8e
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-09 21:56:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,7 +21,10 @@ def play(songs)
   chosen_song = nil 
   
   songs.each_with_index do |song, index|
-    if input == song || input == index + 1
+    if input == song 
+      chosen_song = song
+    end
+    if input == (index + 1).to_s
       chosen_song = song
     end
   end
